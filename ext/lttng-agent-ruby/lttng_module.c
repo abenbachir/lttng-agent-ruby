ba2d1ceee89d0adde2287ec0d6ef9cc074a155e4
Chris Riddoch <riddochc@gmail.com>
2016-02-12 20:19:31 -0700

    Made the conventional set of loglevel tracepoints.

--- ext/lttng-agent-ruby/lttng_module.c
+++ ext/lttng-agent-ruby/lttng_module.c
@@ -1,15 +1,77 @@
-#include <lttng/tracef.h>
-#include "ruby.h"
+#include <ruby.h>
 
-static VALUE t_tracef(VALUE self, VALUE tracept) {
-        tracef("%s", StringValueCStr(tracept));
-        return T_NIL;
+#define TRACEPOINT_CREATE_PROBES
+#define TRACEPOINT_DEFINE
+#include "traces.h"
+
+
+
+VALUE ruby_lttng_trace_info(VALUE self, VALUE mesg) {
+   if (tracepoint_enabled(ruby_log, info)) {
+      tracepoint(ruby_log, info, rb_sourcefile(), rb_sourceline(), StringValuePtr(mesg));
+   }
+   return(Qnil);
 }
+
+
+VALUE ruby_lttng_trace_notice(VALUE self, VALUE mesg) {
+   if (tracepoint_enabled(ruby_log, notice)) {
+      tracepoint(ruby_log, notice, rb_sourcefile(), rb_sourceline(), StringValuePtr(mesg));
+   }
+   return(Qnil);
+}
+
+
+VALUE ruby_lttng_trace_warning(VALUE self, VALUE mesg) {
+   if (tracepoint_enabled(ruby_log, warning)) {
+      tracepoint(ruby_log, warning, rb_sourcefile(), rb_sourceline(), StringValuePtr(mesg));
+   }
+   return(Qnil);
+}
+
+
+VALUE ruby_lttng_trace_err(VALUE self, VALUE mesg) {
+   if (tracepoint_enabled(ruby_log, err)) {
+      tracepoint(ruby_log, err, rb_sourcefile(), rb_sourceline(), StringValuePtr(mesg));
+   }
+   return(Qnil);
+}
+
+
+VALUE ruby_lttng_trace_crit(VALUE self, VALUE mesg) {
+   if (tracepoint_enabled(ruby_log, crit)) {
+      tracepoint(ruby_log, crit, rb_sourcefile(), rb_sourceline(), StringValuePtr(mesg));
+   }
+   return(Qnil);
+}
+
+
+VALUE ruby_lttng_trace_alert(VALUE self, VALUE mesg) {
+   if (tracepoint_enabled(ruby_log, alert)) {
+      tracepoint(ruby_log, alert, rb_sourcefile(), rb_sourceline(), StringValuePtr(mesg));
+   }
+   return(Qnil);
+}
+
+
+VALUE ruby_lttng_trace_emerg(VALUE self, VALUE mesg) {
+   if (tracepoint_enabled(ruby_log, emerg)) {
+      tracepoint(ruby_log, emerg, rb_sourcefile(), rb_sourceline(), StringValuePtr(mesg));
+   }
+   return(Qnil);
+}
+
 
 VALUE lttng_module;
 
 void Init_lttng_module() {
   lttng_module = rb_define_module("LTTng");
-  rb_define_module_function(lttng_module, "tracef", t_tracef, 1);
+  rb_define_singleton_method(lttng_module, "info",    ruby_lttng_trace_info, 1);
+  rb_define_singleton_method(lttng_module, "notice",  ruby_lttng_trace_notice, 1);
+  rb_define_singleton_method(lttng_module, "warning", ruby_lttng_trace_warning, 1);
+  rb_define_singleton_method(lttng_module, "err",     ruby_lttng_trace_err, 1);
+  rb_define_singleton_method(lttng_module, "crit",    ruby_lttng_trace_crit, 1);
+  rb_define_singleton_method(lttng_module, "alert",   ruby_lttng_trace_alert, 1);
+  rb_define_singleton_method(lttng_module, "emerg",   ruby_lttng_trace_emerg, 1);
 }
 
---
+++ ext/lttng-agent-ruby/traces.h
@@ -0,0 +1,103 @@
+#undef TRACEPOINT_PROVIDER
+#define TRACEPOINT_PROVIDER ruby_log
+#
+#undef TRACEPOINT_INCLUDE
+#define TRACEPOINT_INCLUDE "./traces.h"
+
+#if !defined(_TRACEPOINT_UST_RUBY_LOG) || defined(TRACEPOINT_HEADER_MULTI_READ)
+#define _TRACEPOINT_UST_RUBY_LOG
+
+#include "ruby.h"
+#include <lttng/tracepoint.h>
+
+
+
+TRACEPOINT_EVENT(ruby_log,
+                 info,
+                 TP_ARGS(const char*, t_sourcefile,
+                         int, t_sourceline,
+                         char*, t_message),
+                 TP_FIELDS(ctf_string(sourcefile, t_sourcefile)
+                           ctf_integer(int, sourceline, t_sourceline)
+                           ctf_string(message, t_message)))
+TRACEPOINT_LOGLEVEL(ruby_log, info, TRACE_INFO)
+
+
+
+TRACEPOINT_EVENT(ruby_log,
+                 notice,
+                 TP_ARGS(const char*, t_sourcefile,
+                         int, t_sourceline,
+                         char*, t_message),
+                 TP_FIELDS(ctf_string(sourcefile, t_sourcefile)
+                           ctf_integer(int, sourceline, t_sourceline)
+                           ctf_string(message, t_message)))
+TRACEPOINT_LOGLEVEL(ruby_log, notice, TRACE_NOTICE)
+
+
+
+TRACEPOINT_EVENT(ruby_log,
+                 warning,
+                 TP_ARGS(const char*, t_sourcefile,
+                         int, t_sourceline,
+                         char*, t_message),
+                 TP_FIELDS(ctf_string(sourcefile, t_sourcefile)
+                           ctf_integer(int, sourceline, t_sourceline)
+                           ctf_string(message, t_message)))
+TRACEPOINT_LOGLEVEL(ruby_log, warning, TRACE_WARNING)
+
+
+
+TRACEPOINT_EVENT(ruby_log,
+                 err,
+                 TP_ARGS(const char*, t_sourcefile,
+                         int, t_sourceline,
+                         char*, t_message),
+                 TP_FIELDS(ctf_string(sourcefile, t_sourcefile)
+                           ctf_integer(int, sourceline, t_sourceline)
+                           ctf_string(message, t_message)))
+TRACEPOINT_LOGLEVEL(ruby_log, err, TRACE_ERR)
+
+
+
+TRACEPOINT_EVENT(ruby_log,
+                 crit,
+                 TP_ARGS(const char*, t_sourcefile,
+                         int, t_sourceline,
+                         char*, t_message),
+                 TP_FIELDS(ctf_string(sourcefile, t_sourcefile)
+                           ctf_integer(int, sourceline, t_sourceline)
+                           ctf_string(message, t_message)))
+TRACEPOINT_LOGLEVEL(ruby_log, crit, TRACE_CRIT)
+
+
+
+TRACEPOINT_EVENT(ruby_log,
+                 alert,
+                 TP_ARGS(const char*, t_sourcefile,
+                         int, t_sourceline,
+                         char*, t_message),
+                 TP_FIELDS(ctf_string(sourcefile, t_sourcefile)
+                           ctf_integer(int, sourceline, t_sourceline)
+                           ctf_string(message, t_message)))
+TRACEPOINT_LOGLEVEL(ruby_log, alert, TRACE_ALERT)
+
+
+
+TRACEPOINT_EVENT(ruby_log,
+                 emerg,
+                 TP_ARGS(const char*, t_sourcefile,
+                         int, t_sourceline,
+                         char*, t_message),
+                 TP_FIELDS(ctf_string(sourcefile, t_sourcefile)
+                           ctf_integer(int, sourceline, t_sourceline)
+                           ctf_string(message, t_message)))
+TRACEPOINT_LOGLEVEL(ruby_log, emerg, TRACE_EMERG)
+
+
+
+#endif /* _TRACEPOINT_UST_RUBY_LOG */
+
+/* This part must be outside ifdef protection */
+#include <lttng/tracepoint-event.h>
+
