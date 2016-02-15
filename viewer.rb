789de528af36f8b0210d7af7c202bf251da8eb83
Chris Riddoch <riddochc@gmail.com>
2016-02-15 01:05:06 -0700

    Parsing the output of 'lttng view', until I have a proper binding to babeltrace

---
+++ viewer.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/env ruby
+
+class Trace
+  attr_accessor :op, :context, :filename, :line, :method
+  def initialize(op, context, filename, line, method)
+    @op, @context, @filename, @line, @method = op, context, filename, line, method
+  end
+end
+
+class LTTngTraces
+  def each
+    rx = /^\[(?<time>[^\]]+)\].*TracePoint:(?<data>.*)>" }/
+    IO.popen(["lttng", "view"], 'r') do |io|
+      io.each_line.with_index do |line, i|
+        m = rx.match(line)
+        time = m['time']
+        op_context = /(?<op>([bc]_)?(call|return|end|line|class))( `(?<context>[^']+)')?/
+
+        parts = /#{op_context}@(?<filename>.+?):(?<line>\d+)( in `(?<method>[^']+)')?$/.match(m['data'])
+        if parts.nil?
+          raise "OOPS: #{m[2]}"
+        end
+
+        trace = Trace.new(parts['op'], parts['context'], parts['filename'], parts['line'], parts['method'])
+        yield(trace)
+      end
+    end
+  end
+end
+
+LTTngTraces.new.each {|t|
+  puts t.inspect
+}
