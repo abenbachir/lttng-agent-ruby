a7d72f51211df140ffbd37ada1c140d35a0f5ebf
Chris Riddoch <riddochc@gmail.com>
2016-02-14 22:35:15 -0700

    Some initial work on a logger-style class.

---
+++ lib/lttng-agent-ruby/_codegen.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby
+
+require 'erb'
+
+# From syslog names to lttng names...
+severities = {"emergency" => "emerg",
+              "alert" => "alert",
+              "critical" => "crit",
+              "error" => "err",
+              "warning" => "warning",
+              "notice" => "notice",
+              "info" => "info"}
+
+erb = ERB.new(DATA.read, nil, "%<>")
+
+severities.each_pair.with_index {|pair, i|
+  log, ltt = *pair
+  puts erb.result(binding)
+}
+
+__END__
+  def <%= log %>(msg = nil, progname = nil, &b)
+    LTTng.<%= ltt %>(formatter.call(:<%= log %>, 1, progname, (msg || b.call)))
+  end
+
--- lib/lttng-agent-ruby/logger.rb
+++ lib/lttng-agent-ruby/logger.rb
@@ -1,46 +1,47 @@
-require 'logger'
+#!/usr/bin/env ruby
 
-class Log
-  class Formatter
-    def call severity, time, progname, msg
-      msg = msg.to_s.strip.gsub!(/\e\[[0-9;]*m/, '')
+require_relative "../../ext/lttng-agent-ruby/lttng_module"
+
+class LTTngLogger
+  attr_accessor :use_inspect
+  def initialize
+    @use_inspect = false
+  end
+
+  def debug(msg = nil, progname = nil, &b)
+    LTTng.notice(form_message(msg, &b))
+  end
+  def info(msg = nil, progname = nil, &b)
+    LTTng.info(form_message(msg, &b))
+  end
+  def warn(msg = nil, progname = nil, &b)
+    LTTng.warning(form_message(msg, &b))
+  end
+  def error(msg = nil, progname = nil, &b)
+    LTTng.err(form_message(msg, &b))
+  end
+  def fatal(msg = nil, progname = nil, &b)
+    LTTng.crit(form_message(msg, &b))
+  end
+  def unknown(msg = nil, progname = nil, &b)
+    LTTng.alert(form_message(msg, &b))
+  end
+
+  private
+  # ordered in, I hope, likelihood of use?
+  def form_message(msg = nil, &blk)
+    if !@use_inspect && msg
+      msg
+    elsif @use_inspect && msg
+      msg.inspect
+    elsif !@use_inspect && !blk.nil?
+      blk.call()
+    else # @use_inspect && !blk.nil?
+      blk.call().inspect
     end
   end
-
-  attr_accessor :level, :formatter, :facility
-
-  def initialize(program_name = "ruby", facility = nil)
-    @level = ::Logger::DEBUG
-    @formatter = Formatter.new
-    @program_name = program_name
-    # facility is ignored. There's really not much use for it.
-  end
-
-  def self.make_methods meth
-    level = ::Logger.const_get(meth.upcase)
-    eval <<-EOM, nil, __FILE__, __LINE__ + 1
-      def #{meth}(message = nil, &block)
-        add(#{level}, message, &block)
-      end
-
-      def #{meth}?
-        @level <= #{level}
-      end
-    EOM
-  end
-
-  Logger::Severity::constants.each do |severity|
-    make_methods severity.downcase
-  end
-
-  def add(severity, message = nil, progname = nil, &block)
-    severity ||= ::Logger::UNKNOWN
-    @level <= severity and
-      raw_trace( (LEVEL_MAP[severity] | "info") + ":" + formatter.call(severity, Time.now, progname, (message || block.call)) )
-    true
-  end
-
-  def raw_trace(s)
-    LTTng.tracef(s)
-  end
 end
+
+log = LTTngLogger.new
+log.info("Hello, tracelog watchers.")
+
