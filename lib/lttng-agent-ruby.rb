5c4c6125fadb00c098c054eb06d82d5c9d5ed095
Chris Riddoch <riddochc@gmail.com>
2016-02-16 03:07:19 -0700

    Don't make users require so much, it's not exactly a huge amount of code here.

--- lib/lttng-agent-ruby.rb
+++ lib/lttng-agent-ruby.rb
@@ -1,2 +1,6 @@
 require_relative "../ext/lttng-agent-ruby/lttng_module"
 
+require_relative "lttng_agent_ruby/logger"
+require_relative "lttng_agent_ruby/viewer"
+require_relative "lttng_agent_ruby/version"
+
