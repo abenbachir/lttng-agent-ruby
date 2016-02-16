566e3a590c42d5279a9ffc6de978642873fa8f39
Chris Riddoch <riddochc@gmail.com>
2016-02-16 03:45:45 -0700

    Working around bundler issue.

--- lib/lttng-agent-ruby.rb
+++ lib/lttng-agent-ruby.rb
@@ -1,4 +1,15 @@
-require "lttng_module"
+begin
+  require "lttng_module"
+rescue LoadError
+  begin
+    puts "Working around bundler issue of not copying compiled extensions into lib from gems built from git."
+    puts "see: https://github.com/bundler/bundler/issues/3014"
+    FileUtils.cp "../ext/lttng-agent-ruby/lttng_module.so", "lttng_module.so"
+    require "lttng_module"
+  rescue LoadError
+    puts "Fine, I give up."
+  end
+end
 
 require_relative "lttng-agent-ruby/logger"
 require_relative "lttng-agent-ruby/viewer"
